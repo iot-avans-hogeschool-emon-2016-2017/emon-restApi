8eacd0ce22ff5200fb224b277a84d0380010c586
Remco <Remco.boys@gmail.com>
2017-03-15 13:44:27 +0100

    inital commit

---
+++ index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var cool = require('cool-ascii-faces');
+
+var api = express();
+
+api.set('port', (process.env.PORT || 5000));
+
+api.get('/', function(request, response) {
+  const responseString = "Welcome by the restapi for measurements: ";
+
+  response.send(responseString+cool());
+});
+
+api.listen(api.get('port'), function () {
+  console.log('Node app is running on port', api.get('port'));
+});
