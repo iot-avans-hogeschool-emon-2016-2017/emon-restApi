b984dd7661811329fc2ac52749374d964d641ac8
Remco <Remco.boys@gmail.com>
2017-03-22 15:29:10 +0100

    install default packages and remove some dead code

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 var express = require('express');
-var cool = require('cool-ascii-faces');
 
 var api = express();
 
 api.set('port', (process.env.PORT || 5000));
 
 api.get('/', function(request, response) {
-  const responseString = "Welcome by the restapi for measurements: ";
+  const responseString = "Welcome by the restapi for measurements";
 
-  response.send(responseString+cool());
+  response.send(responseString);
 });
 
 api.listen(api.get('port'), function () {
