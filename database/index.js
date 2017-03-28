b71f101f29b3ff1f9a0e1dc322450fde01590f2d
Remco <Remco.boys@gmail.com>
2017-03-28 10:35:50 +0200

    can execute query's multiple times

--- database/index.js
+++ database/index.js
@@ -1,17 +1,46 @@
 var mysql = require('mysql');
 const config = require('./config');
+var exports = module.exports = {};
 
-const connection = mysql.createConnection(config);
+const makeConnection = function () {
+  return mysql.createConnection(config);
+};
 
-connection.connect();
+exports.executeQuery = function (query, callback_results) {
+  const connection = makeConnection();
 
-connection.query('select * from measurements', function (err, results, fields) {
-  if (err) {
-    console.log(err);
-    throw err;
+  connection.query(query, function (err, results) {
+    if (err) {
+      console.log(err);
+      sendResult(failedQuery(err));
+    } else {
+      sendResult(successQuery(results));
+    }
+  });
+
+  function sendResult(results) {
+    if (typeof callback_results !== "undefined") {
+      results = JSON.parse(JSON.stringify(results));
+
+      callback_results(results);
+    }
   }
 
-  console.log(results, "---------\n", fields);
-});
+  connection.end();
+};
 
-connection.end();
+const failedQuery = function (err) {
+  return {
+    "status": 400,
+    "result":err
+  };
+};
+
+const successQuery = function (results) {
+  return {
+    "status": 200,
+    "result": results
+  }
+};
+
+
