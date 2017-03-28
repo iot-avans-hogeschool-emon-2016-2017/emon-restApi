3011a28729a3b333bae6328e08cec95bdcc4bf95
Remco <Remco.boys@gmail.com>
2017-03-28 15:22:57 +0200

    Insert measurement to database

--- routes/measurement.js
+++ routes/measurement.js
@@ -1,3 +1,6 @@
+var database = require('../database');
+var _ = require('lodash');
+
 const measurement = function(req, res) {
   const userId =    req.app.get('user').id;
   const timestamp = req.body.timestamp  || '';
@@ -5,11 +8,10 @@ const measurement = function(req, res) {
   const location =  req.body.location   || '';
 
   if (!userId || !timestamp || !value) {
-    badPost(res);
+    failurePost(res);
     return;
   }
 
-  //object to send to database
   const measurement = {
     "users_id": userId,
     "timestamp": timestamp,
@@ -17,17 +19,62 @@ const measurement = function(req, res) {
     "value": value
   };
 
-  /*@TODO send measurement to database*/
-
-  res.status(201).json({
-    "message": "measurement is posted"
+  addMeasurementToDatabase(measurement, function (response) {
+    if (response.status === 200) {
+      successPost(res)
+    } else {
+      failurePost(res);
+    }
   });
 };
 
-function badPost(res) {
+function addMeasurementToDatabase(measurement, callback) {
+  const query = buildQuery(measurement);
+
+  database.executeQuery(query, function (response) {
+    if (callback) callback(response);
+  });
+}
+
+function buildQuery(measurement) {
+  var query = {
+    prefix_keys: 'INSERT INTO measurements',
+    keys: [],
+    prefix_values: 'VALUES',
+    values: []
+  };
+
+  _.mapKeys(measurement, function (value, key) {
+    if (!!value) {
+      query.keys.push(key);
+      query.values.push(value);
+    }
+  });
+
+  query.keys =   "("+_.join(query.keys,   ", ")+")";
+  query.values = "("+_.join(_.map(query.values, quote), ", ")+")";
+
+  function quote(key) {
+    if (_.isNumber(key)) return key;
+
+    return "'"+key+"'";
+  }
+
+  query = _.join(_.values(query), " ");
+
+  return query;
+}
+
+function failurePost(res) {
   res.status(400).json({
-    "message": "missing content"
+    "message": "posting measurement went wrong"
   })
+}
+
+function successPost(res) {
+  res.status(200).json({
+    "message": "posting measurement went well"
+  });
 }
 
 module.exports = measurement;
